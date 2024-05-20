186db2a2090065c6abf8887f6a0eb4395dc7f42b
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-20 23:55:48 +0300

    Add fallBackAddress to solve address undefined error

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -6,14 +6,18 @@ async function deployFundMe({ deployments, getNamedAccounts }) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
-  
+
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"] ;
+
+    const fallBackAddress = "0x694AA1769357215DE4FAC081bf1f309aDC325306";
+
     const fundMe = await deploy("FundMe", {
       from: deployer,
-      args: [ethUsdPriceFeedAddress],
+      args: [ethUsdPriceFeedAddress || fallBackAddress],
       log: true
     });
   }
   
   module.exports = deployFundMe;
+  module.exports.tags = ["all","sepolia"];
 
